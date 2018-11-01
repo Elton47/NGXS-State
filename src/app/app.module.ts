3b87a3fc0354ffaa91c2ed229b3fbf241f8db4f0
Elton Memishaj <eltoni.memishi@live.com>
2018-11-01 21:56:07 +0100

    + Project Init with NGXS.

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgxsModule } from '@ngxs/store';
+import { RouterState } from './shared/router.state';
+import { AppState } from './shared/app.state';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgxsModule.forRoot([
+      RouterState,
+      AppState
+    ]),
+    NgxsReduxDevtoolsPluginModule.forRoot(),
+    NgxsLoggerPluginModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/shared/app.actions.ts
@@ -0,0 +1,4 @@
+export class SetUsername {
+  static readonly type = '[FileName] set username';
+  constructor(public payload: string) {}
+}
